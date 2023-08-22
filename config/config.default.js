d546845747313eba98b240a7c9bbfacaabcd0b2e
杨可可 <kecoyo@163.com>
2023-08-22 17:39:26 +0800

    fixbug: sequelize时区设置无效；

--- config/config.default.js
+++ config/config.default.js
@@ -85,7 +85,7 @@ module.exports = (appInfo) => {
       paranoid: true, // 启用软删除
       timestamps: true, // 添加create,update,delete时间戳
     },
-    timezone: '+8:00', // 由于orm用的UTC时间，这里必须加上东八区，否则取出来的时间相差8小时
+    timezone: '+08:00', // 由于orm用的UTC时间，这里必须加上东八区，否则取出来的时间相差8小时
   };
 
   config.jwt = {
